9659302c36a818e79c3cdf71f41c2e46d50c72d0
Ravi Gautam <rvgautam59@gmail.com>
2025-08-15 18:15:10 +0530

    update doubts

--- app/api/doubts/[id]/solutions/route.js
+++ app/api/doubts/[id]/solutions/route.js
@@ -2,7 +2,7 @@
 import { NextResponse } from "next/server";
 import { getServerSession } from "next-auth/next";
 import connectMongoDB from "@/lib/mongodb";
-import Doubt from "../../../models/Doubt";
+import Doubt from "@/models/Doubt";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import mongoose from "mongoose";
 
